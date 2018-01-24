4454c21a90e4b4122ec49d31ace110a1adfbd4e4
balder2046 <void1981@gmail.com>
2018-01-25 02:27:47 +0800

    remove some temp code

--- LandmarkServer/Program.cs
+++ LandmarkServer/Program.cs
@@ -11,22 +11,7 @@ namespace LandmarkServer
     {
         static void Main(string[] args)
         {
-            MemoryStream memStream = new MemoryStream(256);
-            BinaryWriter writer = new BinaryWriter(memStream);
-            
-            
-            writer.Write(5L);
-            byte[] bigbuf = memStream.ToArray();
-            System.Console.WriteLine("the bytes is " + bigbuf.Length);
-
-            writer.Write(1.0f);
-            memStream.Seek(0, SeekOrigin.Begin);
-            writer.Seek(0, SeekOrigin.Begin);
-            memStream.SetLength(0);
-            byte[] floatbuf = memStream.ToArray();
-            System.Console.WriteLine("the bytes is " + floatbuf.Length);
-            
-
+          
             PositionServer server = new PositionServer();
             server.StartServer(10001);
             Console.ReadKey();
