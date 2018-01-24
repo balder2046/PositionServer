7e23b383f16d71c85ef1f6e65df7f10c8f1dd9b8
balder2046 <void1981@gmail.com>
2018-01-25 02:29:25 +0800

    modify the window size

--- DemoClient/Form1.Designer.cs
+++ DemoClient/Form1.Designer.cs
@@ -46,7 +46,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(45, 185);
+            this.button1.Location = new System.Drawing.Point(164, 178);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(195, 23);
             this.button1.TabIndex = 1;
@@ -66,7 +66,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(553, 261);
             this.Controls.Add(this.labelPose);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.labelStatus);
