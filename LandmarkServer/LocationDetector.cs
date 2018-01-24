4877dd0f192f209f72ff5d52aa197a9839e7290c
zhaolei <zhaolei@objectgames.com>
2018-01-24 20:26:29 +0800

    添加位置生成器

---
+++ LandmarkServer/LocationDetector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace LandmarkServer
+
+{
+    class PoseInfo
+    {
+        public double fx;
+        public double fz;
+        public double dirx;
+        public double dirz;
+        public void Write(BinaryWriter writer)
+        {
+            writer.Write(fx);
+            writer.Write(fz);
+            writer.Write(dirx);
+            writer.Write(dirz);
+        }
+    }
+    class LocationDetector
+    {
+        public LocationDetector()
+        {
+            memStream = new MemoryStream(256);
+            writer = new BinaryWriter(memStream);
+        }
+        MemoryStream memStream;
+        BinaryWriter writer;
+        PoseInfo GetPoseInfo()
+        {
+            return new PoseInfo();
+        }
+        public byte[] getLocationData()
+        {
+            memStream.SetLength(0);
+            writer.Seek(0, SeekOrigin.Begin);
+            writer.Write(0);
+            PoseInfo info = GetPoseInfo();
+            info.Write(writer);
+            return memStream.ToArray();
+        }
+    }
+}
--- LandmarkServer/Program.cs
+++ LandmarkServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace LandmarkServer
     {
         static void Main(string[] args)
         {
+            MemoryStream memStream = new MemoryStream(256);
+            BinaryWriter writer = new BinaryWriter(memStream);
+            
+            
+            writer.Write(5L);
+            byte[] bigbuf = memStream.ToArray();
+            System.Console.WriteLine("the bytes is " + bigbuf.Length);
+
+            writer.Write(1.0f);
+            memStream.Seek(0, SeekOrigin.Begin);
+            writer.Seek(0, SeekOrigin.Begin);
+            memStream.SetLength(0);
+            byte[] floatbuf = memStream.ToArray();
+            System.Console.WriteLine("the bytes is " + floatbuf.Length);
+            
+
             PositionServer server = new PositionServer();
             server.StartServer(10001);
             Console.ReadKey();
